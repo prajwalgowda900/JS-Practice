6e198550c67873e0cb1224ae6d5d32bb359dac7d
prajwalgowda900 <gprajwal822@gmail.com>
2023-11-23 16:46:43 +0530

    Data

---
+++ DataTypes/data-1.js
@@ -0,0 +1,17 @@
+var   a =1
+var   b =102
+var   c ="103"
+var   d =104
+var   e =105
+var   f =106
+var   i =109
+
+
+
+console.log(a+b)
+console.log(a+c)
+console.log(a+d)
+console.log(a+e)
+console.log(a+f)
+console.log(a+i)
+console.log("Good Morning Prajwal")
---
+++ DataTypes/data.js
@@ -0,0 +1,15 @@
+var product_Name="iphone 12"  ;
+var price=59999;
+var rating=3.2;
+var discount=true;
+var stock;
+
+
+console.log(typeof(product_Name) ) ;
+console.log(typeof(price) ) ;
+console.log(typeof(rating) ) ;
+console.log(typeof(discount) ) ;
+console.log(typeof(stock) ) ;
+
+
+
