527aa721291b7ba15ce01f2e701019093d29216e
Prajwal Gowda <142891668+prajwalgowda900@users.noreply.github.com>
2023-12-09 22:41:45 +0530

    Done

---
+++ js array/forofex.js
@@ -0,0 +1,11 @@
+let enames=["Naveen","Madan","Rahul","Sonia","Priyanka"]
+//print employee Names
+
+/* for(ename of enames){
+    console.log(ename)
+} */
+
+for(var i=0;i<=enames.length-1; i++){
+    //console.log("GM",i)
+    console.log(enames[i])
+}
---
+++ js array/ids.js
@@ -0,0 +1,16 @@
+var ids=[101,102,103,104]
+
+console.log(ids)
+//print array elements
+/* console.log(ids[0])
+console.log(ids[1])
+console.log(ids[2])
+console.log(ids[3])
+
+ */
+
+var i=0
+while(i<=ids.l){
+    console.log(ids[i])
+    i++
+}
