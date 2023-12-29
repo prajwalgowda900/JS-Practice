520e41f0621d5089cf5b3ff46dd5698f32c685b9
Prajwal Gowda <142891668+prajwalgowda900@users.noreply.github.com>
2023-12-29 23:34:11 +0530

    Add files via upload

---
+++ Two.js
@@ -0,0 +1,13 @@
+setTimeout(()=>{
+    console.log("R1")
+},30000)
+
+setTimeout(()=>{
+    console.log("R2")
+},2000)
+setTimeout(()=>{
+    console.log("R3")
+},4000)
+setTimeout(()=>{
+    console.log("R4")
+},10000)
